4a64e3b3838b66ad8535301c41d316d9f309babe
zleodai <awsomeleodai@gmail.com>
2025-02-21 05:27:08 -0800

    some work done on dialogue system

--- LMU-UntitledRPG/Assets/Scripts/UI_DialogueController.cs
+++ LMU-UntitledRPG/Assets/Scripts/UI_DialogueController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,12 +12,36 @@ public class UI_DialogueController : MonoBehaviour
 
     public struct Dialogue {
         public Dialogue(string dialogueData) {
-
             DialogueText = new Dictionary<string, string>();
-            DialogueOptions = new Dictionary<string, string>();
+            DialogueOptions = new Dictionary<string, string[]>();
+            DialogueTransitions = new Dictionary<string, string[]>();
+            DialogueStages = new Dictionary<string, int[]>();
+
+            string[] lineData = dialogueData.Split(new string[] { "{n}" }, StringSplitOptions.None);
+            for (int line = 0; line < lineData.Length; line++) {
+                string data = lineData[line];
+                string[] idSplit = null;
+                if (data.Contains("{:}")) {
+                    idSplit = data.Split(new string[] { "{:}" }, StringSplitOptions.None);
+                } else if (data.Contains("{.}")) {
+                    idSplit = data.Split(new string[] { "{.}" }, StringSplitOptions.None);
+                }
+
+                if (idSplit == null || idSplit.Length <= 1) { break; }
+
+                string[] transitionSplit = idSplit[1].Split(new string[] { "{>}" }, StringSplitOptions.None);
+                
+                // Debug.Log($"{idSplit[0]}: {transitionSplit[0]} -> {transitionSplit[1]}");
+            }
         }
         public Dictionary<string, string> DialogueText { get; }
-        public Dictionary<string, string> DialogueOptions { get; }
+        //DialogueText is the formatted text in a dialogue (note this has to be parsed before being displayed)
+        public Dictionary<string, string[]> DialogueOptions { get; }
+        //DialogueOptions refers to the responses avaliable to a given dialogue
+        public Dictionary<string, string[]> DialogueTransitions { get; }
+        //DialogueTransitions refers to the next dialogue that should be played after a given dialogue ends
+        public Dictionary<string, int[]> DialogueStages { get; }
+        //DialogueStages refers to the stages that this dialogue is present in
     }
 
     void Awake() {
@@ -41,6 +66,6 @@ public class UI_DialogueController : MonoBehaviour
         Debug.Log($"\"{characterName}\" not found in CharacterNames");
     }
     public void StartDialogue(int characterIndex) {
-        
+        throw new NotImplementedException();
     }
 }
