7de7fb85606ed3ed91dd13ab01f3e446d69e6633
Adi Roitburg <112520123+Adiro777@users.noreply.github.com>
2025-02-24 14:25:11 -0800

    Created in battle enemy attributes / attack attributes (#20)

---
+++ LMU-UntitledRPG/Assets/Scripts/ES_EnemyAttack.cs
@@ -0,0 +1,14 @@
+/**
+    Name: Adi Roitburg
+    Date of Creation: 2/23/2025
+    Description: Description of a single enemy attack
+*/
+
+using UnityEngine;
+
+public class ES_EnemyAttack : MonoBehaviour
+{
+    public float damage;
+    public float elementalDamage;
+    public string attackDescription;
+}
