b0dae9dc0e9119cb6cf1d107849bb5cbf488b1a3
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2025-02-25 17:27:40 -0800

    Combat Demo (#22)
    * combat system setup
    
    creating a bunch of prefabs and scripts for combat
    
    * Why does unity hate arrays
    
    * Attack is good enough for now
    
    * some more updates

---
+++ LMU-UntitledRPG/Assets/Scripts/Milo Combat Demo/MiloAttackScriptTemplate.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class MiloAttackScriptTemplate : MonoBehaviour
+{
+    [Header("Attack Components")]
+    public string attackName;
+    public float damage;
+    public string element;
+    public float elementDamage;
+    public string status;
+    public float statusBuildUp;
+    // turn decay is the percentage amount added to the enemy turn meter after the attack is used
+    public float turnDecay;
+    public string attackDescription;
+
+    public static implicit operator MiloAttackScriptTemplate(GameObject v)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ LMU-UntitledRPG/Assets/Scripts/Milo Combat Demo/MiloEnemyScriptTemplate.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MiloEnemyScriptTemplate : MonoBehaviour
+{
+    [Header("Enemy Components")]
+    public string enemyName;
+    public float health;
+    public float maxHealth;
+    public GameObject[] attacks;
+    public int attacksPerTurn;
+    public string[] activeStatusEffects;
+    public int[] statusBuildUps;
+    public string enemyDescription;
+}
