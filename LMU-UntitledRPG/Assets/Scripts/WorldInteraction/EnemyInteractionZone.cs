177a2c48a01eaa073b3b0c6889a0fe53d5fd680a
Xan <83319450+Xan-22@users.noreply.github.com>
2025-02-26 20:29:20 -0800

    removing unused methods

--- LMU-UntitledRPG/Assets/Scripts/WorldInteraction/EnemyInteractionZone.cs
+++ LMU-UntitledRPG/Assets/Scripts/WorldInteraction/EnemyInteractionZone.cs
@@ -6,18 +6,6 @@ public class EnemyInteractionZone : MonoBehaviour
      public GameObject[,] EnemiesEncountered;
      // this could be accessed in the inspector, and have each enemy type added as a game object
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
