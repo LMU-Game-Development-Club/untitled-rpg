0cecbefa95657e23bf509d40cc05cf4c98745218
Xan <83319450+Xan-22@users.noreply.github.com>
2025-03-12 20:37:47 -0700

    dialogue interactions success
    good

---
+++ LMU-UntitledRPG/Assets/Scripts/UI_Dialogue/UI_DialogueManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class UI_DialogueManager : MonoBehaviour
+{
+    public static UI_DialogueManager Instance { get; private set; }
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+}
