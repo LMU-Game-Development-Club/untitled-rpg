c02dd01ee8d9a4d28d7281289e8b8037e7578d47
Xan <83319450+Xan-22@users.noreply.github.com>
2025-04-02 20:33:00 -0700

    restoring old code and scene

--- LMU-UntitledRPG/Assets/Scripts/Milo Combat Demo/MiloEnemyScriptTemplate.cs
+++ LMU-UntitledRPG/Assets/Scripts/Milo Combat Demo/MiloEnemyScriptTemplate.cs
@@ -6,9 +6,8 @@ public class MiloEnemyScriptTemplate : MonoBehaviour
     public string enemyName;
     public float health;
     public float maxHealth;
-
-    public GameObject[] attacks;
-    
+    public GameObject attack1;
+    public GameObject attack2;
     public int attacksPerTurn;
     public string[] activeStatusEffects;
     public int[] statusBuildUps;
